da46cbc74dbfc471567c5e54162478b4bb304653
ayushsingh212 <ayurajput249@gmail.com>
2025-11-11 22:34:20 +0530

    Secured the mail service

--- node-backend/src/utils/sendMail.js
+++ node-backend/src/utils/sendMail.js
@@ -6,13 +6,13 @@ import dotenv from "dotenv"
 // })
 
 const transporter = nodemailer.createTransport({
+  secure: true,
+  host: "smtp.gmail.com",
+  port: 465,
   service: "gmail",
   auth: {
-    type: "OAuth2",
-    user: process.env.GMAIL_USER,
-    clientId: process.env.GMAIL_CLIENT_ID,
-    clientSecret: process.env.GMAIL_CLIENT_SECRET,
-    refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+    user: process.env.COMP_EMAIL,
+    pass: process.env.COMP_PASS,
   },
 });
 
