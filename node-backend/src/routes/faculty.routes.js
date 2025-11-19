a71dab1e1744dbe94eb8cc626180d325374cba50
ayushsingh212 <ayurajput249@gmail.com>
2025-11-19 19:40:32 +0530

    Removed un used faculty route

--- node-backend/src/routes/faculty.routes.js
+++ node-backend/src/routes/faculty.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { createFaculty, deleteFaculty, forgotPassword, getAllFaculty, getFacultyProfile, loginFaculty, requestTimetableUpdate, resetPassword } from "../controllers/faculty.controllers.js";
-import { getFacultyTimetable } from "../controllers/timetable.controllers.js";
+
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -12,7 +12,6 @@ router.get("/getAllFaculties",getAllFaculty);
 router.post("/loginFaculty",loginFaculty);
 router.get("/getFacultyProfile",getFacultyProfile);
 router.delete("/deleteFaculty/:facId",deleteFaculty)
-router.get("/getFacultyTimeTable",getFacultyTimetable);
 router.post("/reqTimetableUpdate",requestTimetableUpdate);
 router.post("/forgotPassword",forgotPassword);
 router.post("/resetPassword",resetPassword);
