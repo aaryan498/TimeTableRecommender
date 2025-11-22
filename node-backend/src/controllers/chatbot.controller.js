ba98c00a9b5d086e88e1840e394a41fbeee5e672
ayushsingh212 <ayurajput249@gmail.com>
2025-11-22 17:08:38 +0530

    Used gemini flash

--- node-backend/src/controllers/chatbot.controller.js
+++ node-backend/src/controllers/chatbot.controller.js
@@ -2,7 +2,9 @@ import asyncHandler from "../utils/asyncHandler.js";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+// Use correct model name
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export const chatBot = asyncHandler(async (req, res) => {
   const { message } = req.body;
